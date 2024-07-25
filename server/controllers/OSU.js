afc36c958543be52d1f773b964dae79554796277
Chaquon <chaquonc@gmail.com>
2024-07-25 14:53:14 -0500

    WIP integrating osu API

--- server/controllers/OSU.js
+++ server/controllers/OSU.js
@@ -1,39 +1,43 @@
-import OsuUserModel from "../models/OsuUserModel";
-import OsuBeatmapUserPerformanceModel from "../models/OsuBeatmapUserPerformanceModel";
-import OsuBeatmapStatsModel from "../models/OsuBeatmapStatsModel";
+// import OsuUserModel from "../models/OsuUserModel";
+// import OsuBeatmapUserPerformanceModel from "../models/OsuBeatmapUserPerformanceModel";
+// import OsuBeatmapStatsModel from "../models/OsuBeatmapStatsModel";
 
-export const getUser = async (req, res) => {
-    const {osuID} = req.body
+// export const getUser = async (req, res) => {
+//     const {osuID} = req.body
 
-    try {
-        const OsuUser = await OsuUserModel.findOne({userID: osuID})
+//     try {
+//         const OsuUser = await OsuUserModel.findOne({userID: osuID})
 
-        res.status(200).json(OsuUser)
-    } catch (error) {
-        res.status(404).json({ message: error.message })
-    }
-}
+//         if (OsuUser) {
 
-export const getBeatmap = async (req, res) => {
-    const {beatmapID} = req.body
+//             res.status(200).json(OsuUser)
+//         }
 
-    try {
-        const beatmap = await OsuBeatmapStatsModel.findOne({beatmapID: beatmapID})
+//     } catch (error) {
+//         res.status(404).json({ message: error.message })
+//     }
+// }
 
-        res.status(200).json(beatmap)
-    } catch (error) {
-        res.status(404).json({ message: error.message })
-    }
-}
+// export const getBeatmap = async (req, res) => {
+//     const {beatmapID} = req.body
 
-export const getBeatmapScore = async (req, res) => {
-    const {scoreID} = req.body
+//     try {
+//         const beatmap = await OsuBeatmapStatsModel.findOne({beatmapID: beatmapID})
 
-    try {
-        const userScore = await OsuBeatmapUserPerformanceModel.findOne({scoreID: scoreID})
+//         res.status(200).json(beatmap)
+//     } catch (error) {
+//         res.status(404).json({ message: error.message })
+//     }
+// }
 
-        res.status(200).json(userScore)
-    } catch (error) {
-        res.status(404).json({ message: error.message})
-    }
-}
+// export const getBeatmapScore = async (req, res) => {
+//     const {scoreID} = req.body
+
+//     try {
+//         const userScore = await OsuBeatmapUserPerformanceModel.findOne({scoreID: scoreID})
+
+//         res.status(200).json(userScore)
+//     } catch (error) {
+//         res.status(404).json({ message: error.message})
+//     }
+// }
--- server/server.js
+++ server/server.js
@@ -5,6 +5,10 @@ import mongoose from "mongoose";
 import { MongoMemoryServer } from "mongodb-memory-server";
 import LOLRoutes from "./routes/LOLRoutes.js"
 import seedAll from "./seeds/index.js"
+// import { Auth, Client } from "osu-web.js";
+import fetch from "node-fetch";
+import { LegacyClient } from "osu-web.js";
+
 
 const app = express();
 dotenv.config();
@@ -17,6 +21,31 @@ app.use("/api/LOL", LOLRoutes)
 const PORT = process.env.PORT || 5050;
 const mongoServer = await MongoMemoryServer.create()
 const DB_URI = process.env.DB_URI || mongoServer.getUri();
+
+// const apiKey = process.env.OSU_API_KEY
+
+// const legacyApi = new LegacyClient(apiKey, {
+//   polyfillFetch: fetch
+// })
+// const user = await legacyApi.getUser({
+//   u: 11962776
+// });
+// console.log(user)
+
+// const clientId = process.env.CLIENT_ID
+// const clientSecret = process.env.CLIENT_SECRET
+// const redirectUri = process.env.REDIRECT_URI
+
+// const auth = new Auth(clientId, clientSecret, redirectUri, {
+//   polyfillFetch: fetch
+// });
+
+// const accessToken = await auth.clientCredentialsGrant();
+// const api = new Client(accessToken, {
+//   polyfillFetch: fetch
+// });
+// const user = await api.users.getUser(Synergy_96)
+// console.log(user)
 
 //testing
 // if (process.env.ENV === "production") {
