76ad52ce0ab160a5c8b1a331652ca8ef94786354
Ruben <rubenramirez121212@gmail.com>
2024-08-07 22:35:46 -0700

    added dynamic SearchBar

--- client/src/router/index.jsx
+++ client/src/router/index.jsx
@@ -1,11 +1,13 @@
 import { createBrowserRouter, Navigate, Outlet } from "react-router-dom";
 import { navbarData } from "../data/NavbarOptions";
 import { lolSidebarData, osuSidebarData } from "../data/SidebarOptions";
+import { lolSearchBarData, osuSearchBarData, tftSearchBarData } from "../data/SearchBarOptions"
 import App from "../App";
 import ContentDisplay from "../components/ContentDisplay/ContentDisplay";
 import LOLSearch from "../components/LeagueOfLegends/LOLSearch";
 import OsuSearch from "../components/Osu/OsuSearch";
 import LOLUserProfile from "../components/LeagueOfLegends/LOLUserProfile";
+import { SearchBar } from "../components/SearchBar/SearchBar";
 
 export const router = createBrowserRouter([
   {
@@ -26,7 +28,7 @@ export const router = createBrowserRouter([
         children: [
           {
             index: true,
-            element: <LOLSearch />,
+            element: <SearchBar searchBarData={lolSearchBarData} />,
           },
           {
             path: "user/:riotID",
@@ -44,10 +46,24 @@ export const router = createBrowserRouter([
         children: [
           {
             index: true,
-            element: <OsuSearch />,
+            element:  <SearchBar searchBarData={osuSearchBarData} />,
           },
         ],
       },
+      {
+        path: "tft",
+        element: (
+          <ContentDisplay sidebarData={lolSidebarData}>
+            <Outlet />
+          </ContentDisplay>
+        ),
+        children: [
+          {
+            index: true,
+            element:  <SearchBar searchBarData={tftSearchBarData} />
+          }
+        ]
+      }
     ],
   },
   {
