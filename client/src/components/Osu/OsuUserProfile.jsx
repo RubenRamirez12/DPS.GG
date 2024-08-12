87be6e86ea95a1fa28d59ae9f5c2edfc4b6fa14e
Ruben <rubenramirez121212@gmail.com>
2024-08-12 09:44:03 -0700

    cleaned repetative css, fixed height and overflow

--- client/src/components/Osu/OsuUserProfile.jsx
+++ client/src/components/Osu/OsuUserProfile.jsx
@@ -3,21 +3,24 @@ import "./OsuUserProfile.css";
 import { useEffect } from "react";
 import { actionClearUser, thunkGetUser } from "../../store/Osu";
 import { useParams } from "react-router-dom";
-import { osuJoinDateDisplay, osuPlayTimeDisplay } from "../../utility/helperFunctions";
+import {
+  osuJoinDateDisplay,
+  osuPlayTimeDisplay,
+} from "../../utility/helperFunctions";
 
 export default function OsuUserProfile() {
   const dispatch = useDispatch();
   const { osuUsername } = useParams();
   let user = useSelector((state) => state.Osu.currentUser);
   let loading = useSelector((state) => state.Osu.loading);
-  let error = useSelector(state => state.Osu.error)
+  let error = useSelector((state) => state.Osu.error);
 
   useEffect(() => {
-    dispatch(thunkGetUser(osuUsername))
+    dispatch(thunkGetUser(osuUsername));
 
     return () => {
-      dispatch(actionClearUser())
-    }
+      dispatch(actionClearUser());
+    };
   }, [dispatch]);
 
   if (loading) {
@@ -40,38 +43,56 @@ export default function OsuUserProfile() {
     <div className="osu-user-profile__div">
       <div className="osu-user-profile__user-info-section">
         <div className="osu-user-profile__user-info-profile">
-        <img className="osu-user-profile__user-info__pfp" src={user.profileIcon} />
-        <span className="osu-user-profile__user-info__username">{user.username}</span>
+          <img
+            className="osu-user-profile__user-info__pfp"
+            src={user.profileIcon}
+          />
+          <span className="osu-user-profile__user-info__username">
+            {user.username}
+          </span>
         </div>
         <div className="osu-user-profile__user-info__joindate">
-          <span className="osu-user-profile__section-header">Account Created</span>
-          <span className="osu-user-profile__section-info">{osuJoinDateDisplay(user.join_date)}</span>
+          <span className="osu-user-profile__section-header">
+            Account Created
+          </span>
+          <span className="osu-user-profile__section-info">
+            {osuJoinDateDisplay(user.join_date)}
+          </span>
         </div>
         <div className="osu-user-profile__user-info__ranks">
           <div className="osu-user-profile__user-info__ranks__global">
-            <span className="osu-user-profile__section-header">GLOBAL RANK</span>
-            <span className="osu-user-profile__section-info">{Number(user.pp_rank).toLocaleString()}</span>
+            <span className="osu-user-profile__section-header">
+              GLOBAL RANK
+            </span>
+            <span className="osu-user-profile__section-info">
+              {Number(user.pp_rank).toLocaleString()}
+            </span>
           </div>
           <div className="osu-user-profile__user-info__ranks__country">
-            <span className="osu-user-profile__section-header">{user.country} RANK</span>
-            <span className="osu-user-profile__section-info">{Number(user.pp_country_rank).toLocaleString()}</span>
+            <span className="osu-user-profile__section-header">
+              {user.country} RANK
+            </span>
+            <span className="osu-user-profile__section-info">
+              {Number(user.pp_country_rank).toLocaleString()}
+            </span>
           </div>
         </div>
         <div className="osu-user-profile__user-info__time-played">
           <span className="osu-user-profile__section-header">TIME PLAYED</span>
-          <span className="osu-user-profile__section-info">{osuPlayTimeDisplay(user.total_seconds_played)}</span>
+          <span className="osu-user-profile__section-info">
+            {osuPlayTimeDisplay(user.total_seconds_played)}
+          </span>
         </div>
         <div className="osu-user-profile__user-info__level">
-          <span className="osu-user-profile__section-header">LEVEL {Math.floor(user.level)}</span>
+          <span className="osu-user-profile__section-header">
+            LEVEL {Math.floor(user.level)}
+          </span>
         </div>
       </div>
+
       <div className="osu-user-profile__user-plays">
-        <div className="osu-user-profile__user-plays__recent">
-
-        </div>
-        <div className="osu-user-profile__user-plays__best">
-
-        </div>
+        <div className="osu-user-profile__user-plays__recent"></div>
+        <div className="osu-user-profile__user-plays__best"></div>
       </div>
     </div>
   );
